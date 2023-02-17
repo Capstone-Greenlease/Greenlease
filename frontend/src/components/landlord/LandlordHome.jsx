78277f747b7b1b88d80a8da3c4d142d1ba242cec
cmachin <carlos.machin@gmail.com>
2023-02-17 14:43:07 -0400

    added cards

--- frontend/src/components/landlord/LandlordHome.jsx
+++ frontend/src/components/landlord/LandlordHome.jsx
@@ -1,5 +1,49 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 function LandlordHome() {
-	return <div>Landlord Home</div>;
+	// fetch
+	const test = [
+		{
+			name: "terrace",
+			location: "close",
+			img: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		},
+		{
+			name: "pueblo",
+			location: "far",
+			img: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		},
+		{
+			name: "guayama",
+			location: "far",
+			img: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		},
+	];
+
+	const cards = test.map((value, index) => {
+		return (
+			<Col key={index}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Img variant="top" src={value.img} />
+					<Card.Body>
+						<Card.Title>{value.name}</Card.Title>
+						<Card.Text>{value.location}</Card.Text>
+						<Button variant="primary">Go somewhere</Button>
+					</Card.Body>
+				</Card>
+			</Col>
+		);
+	});
+
+	return (
+		<>
+			<div>Landlord Home</div>
+			<Row>{cards}</Row>
+		</>
+	);
 }
 
 export default LandlordHome;
