f98a6f9d7beebd71f396c6b9a49e87ef4eb0732b
cmachin <carlos.machin@gmail.com>
2023-03-09 22:51:12 -0400

    implemented fetch api

---
+++ frontend/src/api/fetcher.js
@@ -0,0 +1,38 @@
+export async function getUser({ email, password, type }) {
+	const api = "/data/users.json";
+	const response = await fetch(api);
+	const data = await response.json();
+
+	var result = null;
+	// result = [];
+
+	for (var index in data) {
+		console.log(data[index]);
+		if (
+			data[index].email == email &&
+			data[index].password == password &&
+			data[index].type == type
+		) {
+			result = data[index];
+		}
+	}
+
+	return result;
+}
+
+export async function getProperties(user_id) {
+	const api = "/data/properties.json";
+	const response = await fetch(api);
+	const data = await response.json();
+
+	var result = [];
+
+	for (var index in data) {
+		// console.log(data[index]);
+		if (data[index].landlord_id === user_id) {
+			result.push(data[index]);
+		}
+	}
+
+	return result;
+}
